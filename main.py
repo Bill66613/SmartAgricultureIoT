0a88e51216de90cf758417cbdb87c2290d78e260
Bill <ntmh.bill@gmail.com>
2023-09-17 18:10:50 +0700

    test 1

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+from physical import *
+
 def demo():
     print("I have pasted a snake game code.\nIf you want to try, run python snake.py.")
     print("But I don't think it can be run in the lab development environment.")
 
 def main():
-    print("Hello World!")
+    setDevice1(True)
+    setDevice2(True)
+
+    readTemperature()
+    readMoisture()
+
+    setDevice1(False)
+    setDevice2(False)
+    # counter = 5
+    # while True:
+    #     if counter <= 0:
+    #         counter = 5
+    #         readTemperature()
+    #         readMoisture()
+
+    #     counter = counter - 1
+    #     # Time base for this loop
+    #     time.sleep(1)
 
 if __name__ == "__main__":
+    # demo()
     main()
-    demo()
