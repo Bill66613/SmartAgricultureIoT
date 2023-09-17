9d10578ff86e63d46037fc2520a627631f468b71
Bill <ntmh.bill@gmail.com>
2023-09-17 17:00:28 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def demo():
+    print("I have pasted a snake game code.\nIf you want to try, run python snake.py")
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+    demo()
