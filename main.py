b33345581fea3ab69c57ec96466bec8660db4286
Bill <ntmh.bill@gmail.com>
2023-09-17 17:05:12 +0700

    print more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def demo():
-    print("I have pasted a snake game code.\nIf you want to try, run python snake.py")
+    print("I have pasted a snake game code.\nIf you want to try, run python snake.py.")
+    print("But I don't think it can be run in the lab development environment.")
 
 def main():
     print("Hello World!")
